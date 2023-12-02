82d13260d83776c66cf556467d54d1690ba6917c
Kamil Makarowski <kamil.makarowski@outlook.com>
2023-12-02 13:36:23 +0100

    Day 2 Solution

--- AoC2023/Day2/PartOne.cs
+++ AoC2023/Day2/PartOne.cs
@@ -1,17 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using Utils;
 
 namespace Day2
 {
     internal class PartOne : PartBase
     {
+        Dictionary<string, int> maxCubesDefinedInGame = new Dictionary<string, int>
+        {
+            { "red", 12 },
+            { "green", 13 },
+            { "blue", 14 }
+        };
+
         public override int Run(string[] input)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            foreach(var line in input)
+            {
+                var segments = line.Split(": ");
+                int gameId = int.Parse(segments[0].Split(" ")[1]);
+
+                var maxCubesInGame = GetMaxCubesInGame(segments[1]);
+
+                if(maxCubesInGame.All(x => x.Value <= maxCubesDefinedInGame[x.Key]))
+                {
+                    result += gameId;
+                }
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, int> GetMaxCubesInGame(string game)
+        {
+            string pattern = @"(?<number>\d+)\s+(?<color>\w+)";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(game);
+
+            Dictionary<string, int> cubesMaxValue = new Dictionary<string, int>();
+            foreach (Match match in matches)
+            {
+                int number = int.Parse(match.Groups["number"].Value);
+                string color = match.Groups["color"].Value;
+
+                if (cubesMaxValue.TryGetValue(color, out int currentValue))
+                {
+                    if (number > currentValue)
+                        cubesMaxValue[color] = number;
+                }
+                else
+                    cubesMaxValue[color] = number;
+            }
+
+            return cubesMaxValue;
         }
     }
 }
--- AoC2023/Day2/PartTwo.cs
+++ AoC2023/Day2/PartTwo.cs
@@ -6,7 +6,24 @@ namespace Day2
     {
         public override int Run(string[] input)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            foreach (var line in input)
+            {
+                var segments = line.Split(": ");
+                int gameId = int.Parse(segments[0].Split(" ")[1]);
+
+                var maxCubesInGame = PartOne.GetMaxCubesInGame(segments[1]);
+
+                int allColorsMultiplication = 1;
+                foreach(var maxCubeInGame in maxCubesInGame)
+                {
+                    allColorsMultiplication *= maxCubeInGame.Value;
+                }
+
+                result += allColorsMultiplication;
+            }
+
+            return result;
         }
     }
 }
