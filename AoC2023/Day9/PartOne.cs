68e53dc5d6e9cac0775fd3b9dd5d6feb7078abe3
Kamil Makarowski <kamil.makarowski@outlook.com>
2023-12-11 21:48:23 +0100

    Day 9

---
+++ AoC2023/Day9/PartOne.cs
@@ -0,0 +1,33 @@
+﻿using Utils;
+
+internal class PartOne : PartBase<long>
+{
+    public override long Run(string[] input)
+    {
+        long result = 0;
+
+        foreach(var line in input)
+        {
+            int[] values = line.Split().Select(x => int.Parse(x)).ToArray();
+
+            result += Extrapolate(values);
+        }
+
+        return result;
+    }
+
+    public long Extrapolate(int[] values)
+    {
+        List<int> differences = new List<int>();
+        for (int i = 0; i + 1 < values.Length; i++)
+        {
+            int difference = values[i+1] - values[i];
+            differences.Add(difference);
+        }
+
+        if (differences.All(x => x == 0))
+            return values.Last();
+
+        return values.Last() + Extrapolate(differences.ToArray());
+    }
+}
