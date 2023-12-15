c6c6413cc95eaf4e5e07a389b0cb9e72863bcaab
Kamil Makarowski <kamil.makarowski@outlook.com>
2023-12-15 22:35:16 +0100

    Day 14

---
+++ AoC2023/Day14/PartOne.cs
@@ -0,0 +1,124 @@
+﻿using Utils;
+
+internal class PartOne : PartBase<long>
+{
+    public override long Run(string[] input)
+    {
+        char[,] platform = CreatePlatform(input);
+
+        platform = TiltPlatform(platform);
+
+        Print(platform);
+
+        long result = CalculateWeight(platform);
+
+        return result;
+    }
+
+    public static void Print(char[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static char[,] CreatePlatform(string[] input)
+    {
+        char[,] platform = new char[input[0].Length, input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                platform[i, j] = input[i][j];
+            }
+        }
+
+        return platform;
+    }
+
+    public static char[,] TiltPlatform(char[,] platform)
+    {
+        int width = platform.GetLength(0);
+        int height = platform.GetLength(1);
+
+        char[,] newPlatform = new char[width, height];
+
+        for (int i = 0; i < width; i++)
+        {
+            char[] row = new char[height];
+            for (int j = 0; j < height; j++)
+            {
+                row[j] = platform[j, i];
+            }
+
+            row = TiltColumn(row);
+
+            for (int j = 0; j < row.Length; j++)
+            {
+                newPlatform[j, i] = row[j];
+            }
+        }
+
+        return newPlatform;
+    }
+
+    private static char[] TiltColumn(char[] column)
+    {
+        int shapedRockIndex = 0;
+        int rocks = 0;
+        for (int i = 0; i < column.Length; i++)
+        {
+            if (column[i] == 'O')
+            {
+                rocks++;
+                column[i] = '.';
+                continue;
+            }
+
+            if (column[i] == '#')
+            {
+                for (int j = 0; j < rocks; j++)
+                {
+                    column[shapedRockIndex + j] = 'O';
+                }
+                shapedRockIndex = i + 1;
+                rocks = 0;
+            }
+        }
+
+        for (int j = 0; j < rocks; j++)
+        {
+            column[shapedRockIndex + j] = 'O';
+        }
+
+        return column;
+    }
+
+    public static long CalculateWeight(char[,] platform)
+    {
+        long weight = 0;
+
+        int rows = platform.GetLength(0);
+        int cols = platform.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (platform[i, j] == 'O')
+                    weight += cols - i;
+            }
+        }
+
+        return weight;
+    }
+}
---
+++ AoC2023/Day14/PartTwo.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using Utils;
+using static PartOne;
+
+internal class PartTwo : PartBase<long>
+{
+    public enum WorldOrientation
+    {
+        North,
+        South,
+    }
+
+    public override long Run(string[] input)
+    {
+        long cycles = 1000000000;
+
+
+        char[,] platform = CreatePlatform(input);
+
+        List<char[,]> platforms = new List<char[,]>();
+
+        long result = 0;
+        for (int i = 0; i < cycles; i++)
+        {
+            platform = TiltPlatform(platform);
+
+            platform = Rotate90(platform);
+            platform = TiltPlatform(platform);
+
+            platform = Rotate90(platform);
+            platform = TiltPlatform(platform);
+
+            platform = Rotate90(platform);
+            platform = TiltPlatform(platform);
+
+            platform = Rotate90(platform);
+
+            result = CalculateWeight(platform);
+
+            int index = platforms.FindIndex(x => PlatformsAreEqual(x, platform));
+            if (index != -1)
+            {
+                int patternLength = i - index;
+                int platformIndex = (int)((cycles - index) % patternLength);
+
+                result = CalculateWeight(platforms[index + platformIndex - 1]);
+                break;
+            }
+            platforms.Add(platform);
+        }
+
+        return result;
+    }
+
+    static char[,] Rotate90(char[,] arr)
+    {
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
+
+        char[,] rotatedArray = new char[cols, rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                rotatedArray[j, rows - 1 - i] = arr[i, j];
+            }
+        }
+
+        return rotatedArray;
+    }
+
+    public static bool PlatformsAreEqual(char[,] array1, char[,] array2)
+    {
+        if (array1.GetLength(0) != array2.GetLength(0) || array1.GetLength(1) != array2.GetLength(1))
+        {
+            return false;
+        }
+
+        for (int i = 0; i < array1.GetLength(0); i++)
+        {
+            for (int j = 0; j < array1.GetLength(1); j++)
+            {
+                if (array1[i, j] != array2[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
