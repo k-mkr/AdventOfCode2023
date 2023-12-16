32db67cb2ca39b5d80c1905abb57465786b180e8
Kamil Makarowski <kamil.makarowski@outlook.com>
2023-12-16 21:07:19 +0100

    Day 15 Part 1

---
+++ AoC2023/Day15/PartOne.cs
@@ -0,0 +1,30 @@
+﻿using Utils;
+
+internal class PartOne : PartBase<long>
+{
+    public override long Run(string[] input)
+    {
+        string[] steps = input[0].Split(',');
+
+        long result = 0;
+        foreach (string step in steps)
+        {
+            result += Hash(step);
+        }
+
+        return result;
+    }
+
+    private long Hash(string text)
+    {
+        long seed = 0;
+        long hash = 0;
+        foreach (var c in text)
+        {
+            hash = ((seed + (int)c) * 17) % 256;
+            seed = hash;
+        }
+
+        return hash;
+    }
+}
---
+++ AoC2023/Day15/PartTwo.cs
@@ -0,0 +1,9 @@
+﻿using Utils;
+
+internal class PartTwo : PartBase<long>
+{
+    public override long Run(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
